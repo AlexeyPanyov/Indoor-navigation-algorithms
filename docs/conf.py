3ad1afac748fd608a085f6b051c33c8e8681f1bd
vadim_k <vadim.kartoshkin@mail.ru>
2015-08-23 11:59:22 +0300

    - Changing structure of the Help
    - Updated README.mg

--- docs/conf.py
+++ docs/conf.py
@@ -116,7 +116,7 @@ pygments_style = 'sphinx'
 # Bootstrap THEME html_theme = 'sphinx_bootstrap_theme'
 
 # Read The Docs THEME 
-# html_theme = 'sphinx_navigine_theme'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
--- docs/get_started.rst
+++ docs/get_started.rst
@@ -10,7 +10,7 @@ Contents:
 
 .. toctree::
 
-	step1
-	step2
-	step3
-	step4
+	get_started_pages/step1
+	get_started_pages/step2
+	get_started_pages/step3
+	get_started_pages/step4
